e854dbfded5416723acda05ccbabfa19d980fa5c
yusepjaelani861 <yusepjaelani861@gmail.com>
2022-12-26 18:02:53 +0700

    update route post

--- src/routes/v1/posts/index.ts
+++ src/routes/v1/posts/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import managePosts from "./manage";
+import requestPosts from "./request";
 
 const router = express.Router();
 
+router.use("/", requestPosts);
 router.use("/", managePosts);
 
 export default router;
