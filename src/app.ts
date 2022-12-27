9663e03dfac85c7e46e8c1560b7306759761f00c
yusepjaelani861 <yusepjaelani861@gmail.com>
2022-12-27 10:08:16 +0700

    update route app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { ErrorRequestHandler, Express, NextFunction, Request, Response } from 'express';
+import { sendError } from './libraries/rest';
 import dotenv from 'dotenv';
 import http from 'http';
 import cors from 'cors';
+import fileUpload from 'express-fileupload';
 // import 'express-async-errors';
 import errorHandler from './middleware/error';
 
@@ -12,6 +14,9 @@ const port: number = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+app.use('/public/images/avatar', express.static('src/public/images/avatar'));
 
 app.use(cors({
     origin: '*',
@@ -22,12 +27,20 @@ import authentication from './routes/v1/auth/authentication';
 import user from './routes/v1/users';
 import comunities from './routes/v1/comunities';
 import post from './routes/v1/posts';
-import { sendError } from './libraries/rest';
+import utils from './routes/v1/utils';
+import notification from './routes/v1/notifications';
+import payment from './routes/v1/payments';
+import analytic from './routes/v1/analytics';
 
 app.use('/api/v1/auth', authentication);
 app.use('/api/v1/user', user);
 app.use('/api/v1/group', comunities);
 app.use('/api/v1/post', post);
+app.use('/api/v1/notification', notification);
+app.use('/api/v1/payment', payment);
+app.use('/api/v1/analytics', analytic);
+
+app.use('/api/v1/utils', utils);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
@@ -36,14 +49,13 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use(errorHandler);
 const server = http.createServer(app);
+// app.get('*', (req: Request, res: Response) => {
+//     res.status(404).json(new sendError('Not Found', [], 'NOT_FOUND', 404));
+// })
 
-app.get('*', (req: Request, res: Response) => {
-    res.status(404).json(new sendError('Not Found', [], 'NOT_FOUND', 404));
-})
-
-app.post('*', (req: Request, res: Response) => {
-    res.status(404).json(new sendError('Not Found', [], 'NOT_FOUND', 404));
-})
+// app.post('*', (req: Request, res: Response) => {
+//     res.status(404).json(new sendError('Not Found', [], 'NOT_FOUND', 404));
+// })
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
