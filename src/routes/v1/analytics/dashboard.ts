884e0016f9d8cc3f17d618b902f871875808313a
yusepjaelani861 <yusepjaelani861@gmail.com>
2023-01-05 07:51:16 +0700

    add cache analytic

--- src/routes/v1/analytics/dashboard.ts
+++ src/routes/v1/analytics/dashboard.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { protect, withtoken } from "../../../middleware/auth";
+import cache from "../../../middleware/cache";
 import {
     analytics,
 } from "../../../controllers/v1/analytics/dashboard";
@@ -8,6 +9,6 @@ const router = express.Router();
 
 router
     .route("/:slug")
-    .get(protect, analytics)
+    .get(protect, cache(60), analytics)
 
 export default router;
