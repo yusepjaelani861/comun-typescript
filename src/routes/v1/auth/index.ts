ad52993e160d4a094a5a0cb4364467e53fbfb0de
yusepjaelani861 <yusepjaelani861@gmail.com>
2023-01-19 08:21:58 +0700

    update route auth

--- src/routes/v1/auth/index.ts
+++ src/routes/v1/auth/index.ts
@@ -3,12 +3,12 @@ import express from 'express'
 // import auth from './authentication'
 import auth from './auth';
 import authgoogle from './authgoogle'
-import authfacebook from './authfacebook'
+// import authfacebook from './authfacebook'
 
 const router = express.Router()
 
 router.use('/', auth)
 router.use('/', authgoogle)
-router.use('/', authfacebook)
+// router.use('/', authfacebook)
 
 export default router
