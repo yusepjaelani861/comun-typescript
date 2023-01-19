78365a6e159b6ff828080ae8872070add0d0b95c
yusepjaelani861 <yusepjaelani861@gmail.com>
2023-01-19 07:31:29 +0700

    update max generator otp length

--- src/libraries/helper.ts
+++ src/libraries/helper.ts
@@ -1,7 +1,7 @@
 export const generate_otp = () => {
     var digits = '0123456789';
     let OTP = '';
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 4; i++) {
         OTP += digits[Math.floor(Math.random() * 10)];
     }
     return OTP;
